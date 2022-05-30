f30fad02243110244e76b4df7726f1615349574d
Erfan <erfanamani2000@gmail.com>
2022-05-30 11:25:33 +0430

    Call Start

--- Call Center Simulation Code.py
+++ Call Center Simulation Code.py
@@ -17,6 +17,10 @@ def starting_state():
     state['Shift Status'] = 0
 
     data = dict()
+    # Starting FEL
+    future_event_list = list()
+    future_event_list.append({'Event Type': 'Arrival', 'Event Time': 0, 'Customer': 'C1'})
+    # fel_maker(future_event_list, 'Arrival', 0)
 
     return state, future_event_list, data
 
@@ -30,7 +34,22 @@ def uniform(a, b):
     return a + (b - a) * r
 
 def arrival(future_event_list, state, clock, data, customer):
+    data['Customers'][customer] = dict()
+    data['Customers'][customer]['Arrival Time'] = clock  # track every move of this customer
+    if
 
+    if state['Server Status'] < 3:  # if server is free...
+        state['Server Status'] += 1
+        fel_maker(future_event_list, 'End of Service', clock, customer)
+        data['Customers'][customer]['Time Service Begins'] = clock  # track "every move" of this customer
+        data['Cumulative Stats']['Service Starters'] += 1
+
+    else:  # if server is busy -> wait in queue
+        data['Cumulative Stats']['Area Under Queue Length Curve'] +=\
+            state['Queue Length'] * (clock - data['Last Time Queue Length Changed'])
+        state['Queue Length'] += 1
+        data['Queue Customers'][customer] = clock  # add this customer to the queue
+        data['Last Time Queue Length Changed'] = clock
 
 def end_of_service(future_event_list, state, clock, data, customer):
 
